a99cc45686c75c91f9635f39b52ff0a2451c2da5
Harshvardhan Singh <harshvardhan.singh.rajawat26@gmail.com>
2021-02-03 13:50:47 +0530

    7 task done

--- python/Project_cs.py
+++ python/Project_cs.py
@@ -9,15 +9,15 @@ for i in a:
         fullstop+=1
 print('Commas used in the string are: ',comma)
 print('fullstop used in the string are: ',fullstop)
-x=a.replace(',','')          #here we are using replace function toremove commas and fullstops
+x=a.replace(',','')          #TASK1= finding number of fullstops and commas
 x=x.replace('.','')
 b=x.split(' ')
 c=set(b)
-words=input('Do yo want to know the number of words in the string? Y or N \n')
+words=input('Do yo want to know the number of words in the string? Y or N \n') #TASK2= finding number of words
 if words=='Y':
     count_words=len(b)
     print('Number of words used are: ',count_words)
-alphabets=input('Do you want to know the number of alphabets used in string? Y or N \n')
+alphabets=input('Do you want to know the number of alphabets used in string? Y or N \n') #TASK3= number of alphabets
 count_alphabets=0
 if alphabets=='Y':
     for i in a:
@@ -26,7 +26,7 @@ if alphabets=='Y':
         if ord(i)>=97 and ord(i)<=122:
             count_alphabets+=1
     print('Number of Alphabets used are: ',count_alphabets)
-frequency=input('Do you want to know the frequency of words ? Y or N \n')
+frequency=input('Do you want to know the frequency of words ? Y or N \n') #TASK4= frequency of words
 frequency1={}
 if frequency=='Y':
     for i in c:
@@ -36,14 +36,14 @@ if frequency=='Y':
                 count+=1
         frequency1[i]=count
     print('Frequency of words is: \n',frequency1)
-capital=input('Do you want to know how many words begin with capital alphabets? Y or N \n')
+capital=input('Do you want to know how many words begin with capital alphabets? Y or N \n') #Task5= number of words beginning with capital alphabet
 if capital=='Y':
     count=0
     for i in b:
         if ord(i[0])>=65 and ord(i[0])<=90:
             count+=1
     print('Number of words beginning with capital alphabets are: ',count)
-find=input('Do you want to find any word? Y or N \n')
+find=input('Do you want to find any word? Y or N \n') #TASK6= Finding position of word
 if find=='Y':
     l_find=[]
     find1=input("Enter the word you want to find? \n")
@@ -54,4 +54,9 @@ if find=='Y':
 '''We are giving position of word which is user friendly 
 ex. anand is a very good boy, then here position of 'is' will be 2 instead of giving position
 in technical terms like position will be 6, program should be simple for the user  '''
-        
+reversing=input('Do ou want to see the reversed form of text? Y or N \n') #TASK7= reversing of string
+if reversing=='Y':
+    reversing1=''
+    for i in range(len(a)-1,-1,-1):
+        reversing1=reversing1 + a[i]
+    print('Reversed form of the Text is:\n',reversing1)
